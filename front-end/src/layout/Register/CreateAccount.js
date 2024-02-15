ea92e50d92b7c0c500238b0c5e3426262ab1e0c7
adamawalters <adamawalters@gmail.com>
2024-02-14 22:43:12 -0500

    added form validations to create account

--- front-end/src/layout/Register/CreateAccount.js
+++ front-end/src/layout/Register/CreateAccount.js
@@ -2,16 +2,14 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 function CreateAccount() {
-  const { register, handleSubmit, formState: {errors} } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const navigate = useNavigate();
 
   function handleCreateSubmit(data) {
+    console.log("submitted")
     console.log(data);
-    navigate("/register/activities");
+    // navigate("/register/activities");
   }
-
-  console.log({...register("employee")})
-  
 
   return (
     <div className="page">
@@ -20,20 +18,18 @@ function CreateAccount() {
         <div className="radiogroup-wrapper">
           <div className="formgroup-horizontal">
             <input
-              type="radio"
-              name="employee"
+              type="checkbox"
               id="employee"
-              required
-              {...register("employee")}
+              {...register("employee", { required: true })}
             />
             <label htmlFor="employee">Employee</label>
           </div>
           <div className="formgroup-horizontal">
             <input
-              type="radio"
-              name="employee"
+              type="checkbox"
               id="admin"
-              {...register("admin")}
+              value={"admin"}
+              {...register("admin", { required: true })}
             />
             <label htmlFor="admin">Admin</label>
           </div>
@@ -41,84 +37,80 @@ function CreateAccount() {
         <div className="form-input">
           <label htmlFor="email">Email</label>
           <input
-            required
-            type="text"
-            name="email"
+            type="email"
             placeholder="Enter your email address"
-            {...register("email")}
+            {...register("email", { required: true, minLength: 3 })}
+            minLength={3}
           />
+          {errors["email"] && <p className="form-error-alert">Please check the email</p>}
         </div>
         <div className="form-input">
           <label htmlFor="password">Password</label>
           <input
-            required
             type="password"
-            name="password"
             placeholder="Enter your password"
-            {...register("password")}
+            {...register("password", { required: true, minLength: 5, maxLength: 20 })}
           />
+          {errors["password"] && <p className="form-error-alert">Please check the password</p>}
         </div>
         <div className="input-couple">
           <div className="form-input">
             <label htmlFor="first-name">First Name</label>
             <input
-              required
               type="text"
-              name="first-name"
               placeholder="Enter your first name"
-              {...register("first-name")}
+              {...register("first-name", { required: true })}
             />
+            {errors["first-name"] && <p className="form-error-alert">Please check the first name</p>}
           </div>
           <div className="form-input">
             <label htmlFor="last-name">Last Name</label>
             <input
-              required
               type="text"
-              name="last-name"
               placeholder="Enter your last name"
-              {...register("last-name")}
+              {...register("last-name", { required: true })}
             />
+            {errors["last-name"] && <p className="form-error-alert">Please check the last name</p>}
           </div>
         </div>
         <div className="input-couple">
           <div className="form-input">
             <label htmlFor="date-of-birth">Date of birth</label>
             <input
-              required
               type="date"
-              name="date-of-birth"
-              {...register("date-of-birth")}
+              {...register("date-of-birth", { required: true })}
             />
+            {errors["date-of-birth"] && <p className="form-error-alert">Please select a date of birth</p>}
           </div>
           <div className="form-input">
             <label htmlFor="gender">Gender</label>
-            <select name="gender" required {...register("gender")}>
-             <option value={"female"}>Female</option>
+            <select {...register("gender", { required: true })} defaultValue={""} >
+             <option value={""}>Select</option>
+              <option value={"female"}>Female</option>
               <option value={"male"}>Male</option>
               <option value={"non-binary"}>Non-binary</option>
             </select>
+            {errors["gender"] && <p className="form-error-alert">Please select a gender</p>}
           </div>
         </div>
         <div className="input-couple">
           <div className="form-input">
             <label htmlFor="occupation">Occupation</label>
             <input
-              required
               type="text"
-              name="occupation"
               placeholder="Enter your occupation"
-              {...register("occupation")}
+              {...register("occupation", { required: true })}
             />
+            {errors["occupation"] && <p className="form-error-alert">Please check the occupation</p>}
           </div>
           <div className="form-input">
             <label htmlFor="company">Company</label>
             <input
-              required
               type="text"
-              name="company"
               placeholder="Enter your company"
-              {...register("company")}
+              {...register("company", { required: true })}
             />
+            {errors["company"] && <p className="form-error-alert">Please check the company</p>}
           </div>
         </div>
         <button type="submit" className="submit-form-button">
