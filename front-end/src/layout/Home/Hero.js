b3cd16d3320cff832c61ed5d80918c991e3dbd35
RVPdev <renatovilcahuaman@gmail.com>
2024-02-16 14:32:22 -0600

    added images from figma

--- front-end/src/layout/Home/Hero.js
+++ front-end/src/layout/Home/Hero.js
@@ -1,12 +1,15 @@
+import img1 from "../../assets/landing_page/Landing_Page_image_1.png";
+
 function Hero() {
   return (
     <section className="hero" id="hero">
       <div className="hero-section">
         <div className="info">
           <h1>EMPOWER YOUR TEAM</h1>
-          <h2>CREATING A CULTURE OF MENTAL WEALTH TOGETHER</h2>
+          <h2>Create a culture of mental wealth together</h2>
+          <button> JOIN US TODAY</button>
           <img
-            src="https://placehold.co/400"
+            src={img1}
             alt="centered placeholder"
             className="centered-image"
           />
