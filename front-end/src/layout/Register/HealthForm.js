575344926291c455d0a6af253ebc51780f892aa8
adamawalters <adamawalters@gmail.com>
2024-02-16 00:48:24 -0500

    made all inputs on healthform required

--- front-end/src/layout/Register/HealthForm.js
+++ front-end/src/layout/Register/HealthForm.js
@@ -14,41 +14,42 @@ function HealthForm() {
   }
 
   return (
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
       <h1>Daily Health Tracker</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h4>How are you feeling?</h4>
         <div className="emoji-wrapper">
           <label className="formatted-radio">
             Happy
-            <input type="radio" name="mood" value={1} {...register("mood")} />
+            <input type="radio" name="mood" value={1} {...register("mood", { required: true })} />
             <div>
               <img className="emoji" src={HappyIcon} alt="happy icon" />
             </div>
           </label>
           <label className="formatted-radio">
             Sad
-            <input type="radio" name="mood" value={2} {...register("mood")} />
+            <input type="radio" name="mood" value={2} {...register("mood", { required: true })} />
             <div>
               <img className="emoji" src={SadIcon} alt="sad icon" />
             </div>
           </label>
           <label className="formatted-radio">
             Upset
-            <input type="radio" name="mood" value={3} {...register("mood")} />
+            <input type="radio" name="mood" value={3} {...register("mood", { required: true })} />
             <div>
               <img className="emoji" src={AngryIcon} alt="upset icon" />
             </div>
           </label>
           <label className="formatted-radio">
             Tired
-            <input type="radio" name="mood" value={4} {...register("mood")} />
+            <input type="radio" name="mood" value={4} {...register("mood", { required: true })} />
             <div>
               <img className="emoji" src={TiredIcon} alt="tired icon" />
             </div>
           </label>
           <label className="formatted-radio">
             Meh
-            <input type="radio" name="mood" value={5} {...register("mood")} />
+            <input type="radio" name="mood" value={5} {...register("mood", { required: true })} />
             <div>
               <img className="emoji" src={MehIcon} alt="meh icon" />
             </div>
@@ -59,23 +60,23 @@ function HealthForm() {
         <div className="emoji-wrapper">
           <label>
             >5 hours
-            <input type="radio" name="sleep" value={1} {...register("sleep")} />
+            <input type="radio" name="sleep" value={1} {...register("sleep", { required: true })} />
           </label>
           <label>
             5-7 hours
-            <input type="radio" name="sleep" value={2} {...register("sleep")} />
+            <input type="radio" name="sleep" value={2} {...register("sleep", { required: true })} />
           </label>
           <label>
             8 hours
-            <input type="radio" name="sleep" value={3} {...register("sleep")} />
+            <input type="radio" name="sleep" value={3} {...register("sleep", { required: true })} />
           </label>
           <label>
             9 hours
-            <input type="radio" name="sleep" value={4} {...register("sleep")} />
+            <input type="radio" name="sleep" value={4} {...register("sleep", { required: true })} />
           </label>
           <label>
             10+ hours
-            <input type="radio" name="sleep" value={5} {...register("sleep")} />
+            <input type="radio" name="sleep" value={5} {...register("sleep", { required: true })} />
           </label>
         </div>
 
@@ -83,23 +84,23 @@ function HealthForm() {
         <div className="emoji-wrapper">
           <label>
             1
-            <input type="radio" name="activity" value={1} {...register("activity")} />
+            <input type="radio" name="activity" value={1} {...register("activity", { required: true })} />
           </label>
           <label>
             2
-            <input type="radio" name="activity" value={2} {...register("activity")} />
+            <input type="radio" name="activity" value={2} {...register("activity", { required: true })} />
           </label>
           <label>
             3
-            <input type="radio" name="activity" value={3} {...register("activity")} />
+            <input type="radio" name="activity" value={3} {...register("activity", { required: true })} />
           </label>
           <label>
             4
-            <input type="radio" name="activity" value={4} {...register("activity")} />
+            <input type="radio" name="activity" value={4} {...register("activity", { required: true })} />
           </label>
           <label>
             5
-            <input type="radio" name="activity" value={5} {...register("activity")} />
+            <input type="radio" name="activity" value={5} {...register("activity", { required: true })} />
           </label>
         </div>
 
@@ -107,28 +108,29 @@ function HealthForm() {
         <div className="emoji-wrapper">
           <label>
             1
-            <input type="radio" name="stress" value={1} {...register("stress")} />
+            <input type="radio" name="stress" value={1} {...register("stress", { required: true })} />
           </label>
           <label>
             2
-            <input type="radio" name="stress" value={2} {...register("stress")} />
+            <input type="radio" name="stress" value={2} {...register("stress", { required: true })} />
           </label>
           <label>
             3
-            <input type="radio" name="stress" value={3} {...register("stress")} />
+            <input type="radio" name="stress" value={3} {...register("stress", { required: true })} />
           </label>
           <label>
             4
-            <input type="radio" name="stress" value={4} {...register("stress")} />
+            <input type="radio" name="stress" value={4} {...register("stress", { required: true })} />
           </label>
           <label>
             5
-            <input type="radio" name="stress" value={5} {...register("stress")} />
+            <input type="radio" name="stress" value={5} {...register("stress", { required: true })} />
           </label>
         </div>
 
         <button className="submit-form-button">SUBMIT</button>
       </form>
+    </div>
   );
 }
 
