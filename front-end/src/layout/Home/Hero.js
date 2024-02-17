da79b15c751279b3f33402fe5452e0335cb0858e
RVPdev <renatovilcahuaman@gmail.com>
2024-02-17 00:08:26 -0600

    added funcitnality to buttons

--- front-end/src/layout/Home/Hero.js
+++ front-end/src/layout/Home/Hero.js
@@ -1,13 +1,21 @@
 import img1 from "../../assets/landing_page/Landing_Page_image_1.png";
 
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 function Hero() {
+  let navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path);
+  };
   return (
     <section className="hero" id="hero">
       <div className="hero-section">
         <div className="info">
           <h1>EMPOWER YOUR TEAM</h1>
           <h2>Create a culture of mental wealth together</h2>
-          <button> JOIN US TODAY</button>
+          <button onClick={() => navigateTo("/login")}> JOIN US TODAY</button>
           <img
             src={img1}
             alt="centered placeholder"
--- front-end/src/layout/Home/Ready.js
+++ front-end/src/layout/Home/Ready.js
@@ -1,8 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 function Ready() {
+  let navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path);
+  };
+
   return (
     <section className="hero" id="ready">
       <h2>Ready to get started?</h2>
-      <button>JOIN US TODAY</button>
+      <button onClick={() => navigateTo("/login")}>JOIN US TODAY</button>
     </section>
   );
 }
