2b269026beb3fc61a3f4ca201a00dd6a71b893c4
RVPdev <renatovilcahuaman@gmail.com>
2024-02-16 23:34:23 -0600

    finished 3 section of landing page

--- front-end/src/layout/Home/Help.js
+++ front-end/src/layout/Home/Help.js
@@ -1,9 +1,31 @@
+import img_one from "../../assets/landing_page/Landing_Page_image_3.png";
+import img_two from "../../assets/landing_page/Landing_Page_image_4.png";
+import img_three from "../../assets/landing_page/Landing_page_image_5.png";
+
 function Help() {
-    return(
-        <section className="hero" id="help" >
-            <div>HI</div>
-        </section>
-    );
+  return (
+    <section className="hero stack" id="help">
+      <div className="help-title">
+        <h2>WE CAN HELP</h2>
+      </div>
+      <div className="mood-banner">
+        <div className="sgt-bnr">
+          <img src={img_one} alt="girl sitting" className="img-one" />
+          <p>MOOD TRACKING</p>
+        </div>
+
+        <div className="sgt-bnr">
+          <img src={img_two} alt="man levitating" className="img-two" />
+          <p>STRESS MANAGEMENT</p>
+        </div>
+
+        <div className="sgt-bnr">
+          <img src={img_three} alt="man with light bulb" className="img-three" />
+          <p>PERSONALIZED TIPS</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Help;
