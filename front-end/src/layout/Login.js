0c6ff5e841066a1ca8cd27ac1abd1c4cd6e593d7
RVPdev <renatovilcahuaman@gmail.com>
2024-02-17 23:32:44 -0600

    added log in page

--- front-end/src/layout/Login.js
+++ front-end/src/layout/Login.js
@@ -2,8 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { login } from "../utils/api";
 import "./Register/register.css";
-import LoginImage from "./../assets/login_create/login_image.png"
-
+import LoginImage from "./../assets/login_create/login_image.png";
 
 function Login() {
   const {
@@ -18,7 +17,7 @@ function Login() {
     console.log(data);
     const { user_id, administer_access } = await login(data);
     /*Backend needs to send me employee ID */
-    console.log(`USERID: ${user_id}`)
+    console.log(`USERID: ${user_id}`);
     if (administer_access) {
       navigate(`/admin`);
     } else {
@@ -30,39 +29,49 @@ function Login() {
   return (
     <div className="page">
       <div className="left-register-section">
-        <h1>Welcome back</h1>
-        <div className="register-image-wrapper">
-            <img src={LoginImage} alt="log-in"/>
+        <div className="register-title">
+          <h1>Welcome back!</h1>
         </div>
-      </div>  
-      <form className="styled-form" onSubmit={handleSubmit(submitLogin)}>
-        <h1>Log in</h1>
-        <div className="form-input">
-          <label htmlFor="user_email">Email</label>
-          <input
-            required
-            type="text"
-            name="user_email"
-            placeholder="Enter your email address"
-            {...register("user_email")}
+        <div>
+          <img
+            src={LoginImage}
+            alt="log-in"
+            className="register-image-wrapper"
           />
         </div>
-        <div className="form-input">
-          <label htmlFor="user_password">Password</label>
-          <input
-            required
-            type="password"
-            name="user_password"
-            placeholder="Enter your password"
-            {...register("user_password")}
-          />
-        </div>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <Link className="button-text" to="/register">Create an account</Link>
-        <button type="submit" className="submit-form-button">
-          LOG IN
-        </button>
-      </form>
+      </div>
+      <div className="right-register-section">
+        <form className="styled-form" onSubmit={handleSubmit(submitLogin)}>
+          <h1>Log in</h1>
+          <div className="form-input">
+            <label htmlFor="user_email">Email</label>
+            <input
+              required
+              type="text"
+              name="user_email"
+              placeholder="Enter your email address"
+              {...register("user_email")}
+            />
+          </div>
+          <div className="form-input">
+            <label htmlFor="user_password">Password</label>
+            <input
+              required
+              type="password"
+              name="user_password"
+              placeholder="Enter your password"
+              {...register("user_password")}
+            />
+          </div>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <Link className="btn-text" to="/register">
+            Create an account
+          </Link>
+          <button type="submit" className="submit-form-btn">
+            LOG IN
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
