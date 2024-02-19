aa2519bfc8ec673ab796005ff08b090ff857379e
RVPdev <renatovilcahuaman@gmail.com>
2024-02-19 13:52:37 -0600

    register/create account done

--- front-end/src/layout/Register/CreateAccount.js
+++ front-end/src/layout/Register/CreateAccount.js
@@ -2,83 +2,113 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { createAccount } from "../../utils/api";
-import "./register.css"
+import "./register.css";
 import ErrorAlert from "../ErrorAlert";
+import CreateImage from "../../assets/login_create/create_account_image.png";
 
-
-function CreateAccount({loggedIn, setLoggedIn}) {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+function CreateAccount({ loggedIn, setLoggedIn }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const navigate = useNavigate();
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
   async function handleCreateSubmit(data) {
     console.log(data);
     /* need to submit to DB and receive an employeeId*/
     try {
-      const user_id = await createAccount(data)
-      console.log(`Created user id: ${user_id}`)
+      const user_id = await createAccount(data);
+      console.log(`Created user id: ${user_id}`);
       setLoggedIn(true);
       navigate("/register/more", {
-        replace: true, 
+        replace: true,
         state: {
-          user_id: user_id
-        }
-     });
-
-
+          user_id: user_id,
+        },
+      });
     } catch (error) {
-      setError(error)
+      setError(error);
     }
-
-    
   }
 
   return (
     <div className="page">
-      {error ? <ErrorAlert error={error} /> : null}
-      <form className="styled-form" onSubmit={handleSubmit(handleCreateSubmit)}>
-      <h1>Create an account</h1>
-        <div className="form-input">
-          <label htmlFor="user_email">Email</label>
-          <input
-            type="email"
-            placeholder="Enter your email address"
-            {...register("user_email", { required: true, minLength: 3 })}
-            minLength={3}
-          />
-          {errors["user_email"] && <p className="form-error-alert">Please check the email</p>}
+      <div className="left-register-section">
+      <div className="register-title">
+          <h1>We can help!</h1>
         </div>
-        <div className="form-input">
-          <label htmlFor="user_password">Password</label>
-          <input
-            type="password"
-            placeholder="Enter your password"
-            {...register("user_password", { required: true, minLength: 5, maxLength: 20 })}
+        <div>
+          <img
+            src={CreateImage}
+            alt="register"
+            className="register-image-wrapper second-img"
           />
-          {errors["user_password"] && <p className="form-error-alert">Please check the password</p>}
         </div>
-          <div className="form-input">
+      </div>
+
+      <div className="right-register-section">
+        {error ? <ErrorAlert error={error} /> : null}
+        <form
+          className="styled-form-2"
+          onSubmit={handleSubmit(handleCreateSubmit)}
+        >
+          <h1>Create an account</h1>
+          <div className="form-input-2">
+            <label htmlFor="user_email">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              {...register("user_email", { required: true, minLength: 3 })}
+              minLength={3}
+            />
+            {errors["user_email"] && (
+              <p className="form-error-alert">Please check the email</p>
+            )}
+          </div>
+          <div className="form-input-2">
+            <label htmlFor="user_password">Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              {...register("user_password", {
+                required: true,
+                minLength: 5,
+                maxLength: 20,
+              })}
+            />
+            {errors["user_password"] && (
+              <p className="form-error-alert">Please check the password</p>
+            )}
+          </div>
+          <div className="form-input-2">
             <label htmlFor="user_first-name">First Name</label>
             <input
               type="text"
               placeholder="Enter your first name"
               {...register("user_first_name", { required: true })}
             />
-            {errors["user_first-name"] && <p className="form-error-alert">Please check the first name</p>}
+            {errors["user_first-name"] && (
+              <p className="form-error-alert">Please check the first name</p>
+            )}
           </div>
-          <div className="form-input">
+          <div className="form-input-2">
             <label htmlFor="user_last-name">Last Name</label>
             <input
               type="text"
               placeholder="Enter your last name"
               {...register("user_last_name", { required: true })}
             />
-            {errors["user_last-name"] && <p className="form-error-alert">Please check the last name</p>}
+            {errors["user_last-name"] && (
+              <p className="form-error-alert">Please check the last name</p>
+            )}
           </div>
-        <button type="submit" className="submit-form-button">
-          SUBMIT
-        </button>
-      </form>
+          <button type="submit" className="submit-form-btn-2">
+            SUBMIT
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
