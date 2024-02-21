b0af0f6582eb5c2974cfbbd28d9406331eec493a
adamawalters <adamawalters@gmail.com>
2024-02-21 18:56:01 -0500

    updated admin page to show no employees message and made button cursor pointer

--- front-end/src/layout/Admin/EmployeeTable.js
+++ front-end/src/layout/Admin/EmployeeTable.js
@@ -3,23 +3,31 @@ function EmployeeTable({ employees, handleDelete }) {
     return (
       <tr key={employee.user_id} className="tbl-row">
         <td className="tbl-name">
-          {employee.user_first_name}
-          {" "}
-          {employee.user_last_name}
+          {employee.user_first_name} {employee.user_last_name}
         </td>
 
         <td className="tbl-email">{employee.user_email}</td>
-        <td className="tbl-btn" >
-          <button className="btn-ex" onClick={() => handleDelete(employee)}>Delete</button>
+        <td className="tbl-btn">
+          <button className="btn-ex" onClick={() => handleDelete(employee)}>
+            Delete
+          </button>
         </td>
       </tr>
     );
   });
 
+  const noEmployeesMessage = (
+    <h1>No employees found. Please edit the search query and try again. </h1>
+  );
+
   return (
-    <table className="tbl">
-      <tbody>{rows}</tbody>
-    </table>
+    <>
+      {rows.length ? (
+        <table className="tbl">
+          <tbody>{rows}</tbody>
+        </table>
+      ) : noEmployeesMessage}
+    </>
   );
 }
 
